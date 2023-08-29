c7a0ee8096222c69cf07a8015bc0531238fb91f2
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-08-28 17:43:38 -0700

    user can select dice and number of rolls

--- die_visual.py
+++ die_visual.py
@@ -2,19 +2,30 @@ import pygal
 
 from die import Die
 
-die = Die()
+num_die = int(input("How many dice do you want to roll? "))
+print("How many sides does each dice have?")
+dies = []
+for i in range(num_die):
+    prompt = "Die #" + str(i+1) + ": "
+    sides = int(input(prompt))
+    dies.append(Die(sides))
+num_rolls = int(input("How many times do you want to roll your dice? "))
 
-res = []
-for _ in range(1000):
-    res.append(die.roll())
+roll_values = []
+for _ in range(num_rolls):
+    total = 0
+    for d in dies:
+        total += d.roll()
+    roll_values.append(total)
 
 freq = []
-for val in range(1, die.num_sides+1):
-    freq.append(res.count(val))
+max_val = sum(d.num_sides for d in dies)
+for val in range(num_die, max_val+1):
+    freq.append(roll_values.count(val))
 
 hist = pygal.Bar()
-hist.title = 'Results of rolling one D6 1000 times.'
-hist.x_labels = [str(i+1) for i in range(6)]
+hist.title = 'Results of rolling selected dice ' + str(num_rolls) + ' times.'
+hist.x_labels = [str(i) for i in range(num_die, max_val+1)]
 hist.x_title = 'Result'
 hist.y_title = 'Frequency'
 hist.add('D6', freq)
