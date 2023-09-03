e1e0b04d817722f3272d5224f828f2656ce66422
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-09-02 20:12:05 -0700

    added config for bar graph

--- python_repos.py
+++ python_repos.py
@@ -22,7 +22,17 @@ for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
 
-chart = pygal.Bar(x_label_rotation=45, show_legend=False)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config)
 chart.title = 'Most-Starred Python Projects on Github'
 chart.x_labels = names
 chart.add('', stars)
