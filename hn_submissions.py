9713225f2c7568656e41b1bd926a8d29b22d8c0f
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-09-03 10:12:51 -0700

    added interactive chart

--- hn_submissions.py
+++ hn_submissions.py
@@ -1,3 +1,4 @@
+import pygal
 import requests
 from operator import itemgetter
 
@@ -24,7 +25,30 @@ for submission_id in submission_ids[:30]:
 # sort stories in descending order based on number of comments
 submission_dicts.sort(key=itemgetter('comments'), reverse=True)
 
+names, plot_dicts = [], []
 for submission_dict in submission_dicts:
     print('\nTitle:', submission_dict['title'])
     print('Discussion link:', submission_dict['link'])
     print('Comments:', submission_dict['comments'])
+    names.append(submission_dict['title'])
+    plot_dict = {
+        'value': submission_dict['comments'],
+        'xlink': submission_dict['link']
+    }
+    plot_dicts.append(plot_dict)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config)
+chart.title = 'Most Active Discussions on Hacker News'
+chart.x_labels = names
+chart.add('', plot_dicts)
+chart.render_to_file('hn_submissions.svg')
