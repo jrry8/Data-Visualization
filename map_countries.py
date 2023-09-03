eb4c5cba4ddaa04697189fb98335dcf0cee384db
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-09-03 10:37:42 -0700

    deleted old files

--- map_countries.py
+++
@@ -1,9 +0,0 @@
-import pygal
-
-worldmap_chart = pygal.maps.world.World()
-worldmap_chart.title = 'Americas'
-worldmap_chart.add('North America', ['ca', 'mx', 'us'])
-worldmap_chart.add('Central America', ['bz', 'cr', 'gt', 'hn', 'ni', 'pa', 'sv'])
-worldmap_chart.add('South America', ['ar', 'bo', 'br', 'cl', 'co', 'ec', 'gf',
- 'gy', 'pe', 'py', 'sr', 'uy', 've'])
-worldmap_chart.render_to_file('americas.svg')
