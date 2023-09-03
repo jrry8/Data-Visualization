4210a55ec198f24df2ffd8699aa5b682ccedc39b
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-09-02 20:08:51 -0700

    visualization of popular python repos on github

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,5 @@
 import requests
+import pygal
 
 # make an API call and store the response
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -16,12 +17,13 @@ for key in sorted(repo_dict.keys()):
     print(key) 
 '''
 # examine top repos
-print("\nSelected information about top repos:")
+names, stars = [], []
 for repo_dict in repo_dicts:
-    print('\nName:', repo_dict['name'])
-    print('Owner:', repo_dict['owner']['login'])
-    print('Stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
-    print('Created:', repo_dict['created_at'])
-    print('Udpated:', repo_dict['updated_at'])
-    print('Description:', repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+chart = pygal.Bar(x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on Github'
+chart.x_labels = names
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
